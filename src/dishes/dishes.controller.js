66dba5b007ec9d4bcb22b223ec9ddaa392ac2407
Pranay Appikatla <0pranay.appikatla0@gmail.com>
2021-08-16 19:51:36 -0400

    final commit

--- src/dishes/dishes.controller.js
+++ src/dishes/dishes.controller.js
@@ -1,3 +1,6 @@
+const target = { id: "hello", a: 1, b: 2 };
+const updater = { b: 4, a: 5 };
+
 const path = require("path");
 const { forEach } = require("../data/dishes-data");
 
@@ -6,3 +9,98 @@ const dishes = require(path.resolve("src/data/dishes-data"));
 
 // Use this function to assign ID's when necessary
 const nextId = require("../utils/nextId");
+
+const dishExists = (req, res, next) => {
+  const { dishId } = req.params;
+  const foundDish = dishes.find((dish) => dish.id === dishId);
+
+  if (!foundDish) {
+    return next({
+      status: 404,
+      message: `Dish does not exist: ${dishId}`,
+    });
+  }
+  res.locals.dish = foundDish;
+  next();
+};
+
+const requiredFieldsCheck = (req, res, next) => {
+  const requiredFields = ["name", "description", "price", "image_url"];
+  const data = req.body.data || {};
+  for (const field of requiredFields) {
+    if (!data[field]) {
+      return next({
+        status: 400,
+        message: `Dish must include a ${field}`,
+      });
+    }
+  }
+  res.locals.data = data;
+  next();
+};
+
+const priceValidator = (req, res, next) => {
+  const priceCheck = res.locals.data.price;
+  if (typeof priceCheck !== "number" || priceCheck <= 0) {
+    return next({
+      status: 400,
+      message: "Dish must hav a price that is an integer greater than 0",
+    });
+  }
+  next();
+};
+
+const idValidator = (req, res, next) => {
+  const { data: { id } = {} } = req.body;
+  const { dishId } = req.params;
+  if (id && id !== dishId) {
+    return next({
+      status: 400,
+      message: `Dish id does not match route id. Dish: ${id}, Route: ${dishId}`,
+    });
+  }
+  next();
+};
+
+const list = (req, res, next) => {
+  res.json({ data: dishes });
+};
+
+const read = (req, res, next) => {
+  res.json({ data: res.locals.dish });
+};
+
+const update = (req, res, next) => {
+  if (res.locals.data.hasOwnProperty("id")) {
+    delete res.locals.data.id;
+  }
+  const response = Object.assign(res.locals.dish, res.locals.data);
+  res.json({ data: response });
+};
+
+const create = (req, res, next) => {
+  const { data: { name, description, price, image_url } = {} } = req.body;
+  const newDish = {
+    name,
+    description,
+    price,
+    image_url,
+    id: nextId(),
+  };
+  dishes.push(newDish);
+  res.status(201).json({ data: newDish });
+  next();
+};
+
+module.exports = {
+  list,
+  create: [requiredFieldsCheck, priceValidator, create],
+  read: [dishExists, read],
+  update: [
+    dishExists,
+    requiredFieldsCheck,
+    priceValidator,
+    idValidator,
+    update,
+  ],
+};
--- src/orders/orders.controller.js
+++ src/orders/orders.controller.js
@@ -6,4 +6,133 @@ const orders = require(path.resolve("src/data/orders-data"));
 // Use this function to assigh ID's when necessary
 const nextId = require("../utils/nextId");
 
+const orderExists = (req, res, next) => {
+  const { orderId } = req.params;
+  const foundOrder = orders.find((order) => order.id === orderId);
+
+  if (!foundOrder) {
+    return next({
+      status: 404,
+      message: `Order does not exist: ${orderId}`,
+    });
+  }
+  res.locals.order = foundOrder;
+  next();
+};
+
+const requiredFieldsCheck = (req, res, next) => {
+  const requiredFields = ["deliverTo", "mobileNumber", "dishes"];
+  const data = req.body.data || {};
+  for (const field of requiredFields) {
+    if (!data[field]) {
+      return next({
+        status: 400,
+        message: `Dish must include a ${field}`,
+      });
+    }
+  }
+
+  if (!Array.isArray(data.dishes) || data.dishes.length === 0) {
+    return next({
+      status: 400,
+      message: `Dish must include at least one dish`,
+    });
+  }
+  for (const index in data.dishes) {
+    console.log(index);
+    if (
+      !data.dishes[index].quantity ||
+      data.dishes[index].quantity === 0 ||
+      !Number.isInteger(data.dishes[index].quantity)
+    ) {
+      return next({
+        status: 400,
+        message: `Dish ${index} must have a quantity that is an integer greater than 0`,
+      });
+    }
+  }
+  res.locals.data = data;
+  next();
+};
+
+const idValidator = (req, res, next) => {
+  const { data: { id, status } = {} } = req.body;
+  const { orderId } = req.params;
+
+  if (res.locals.order.status === "delivered") {
+    return next({
+      status: 400,
+      message: "A delivered order cannot be changed",
+    });
+  }
+
+  if (id && id !== orderId) {
+    return next({
+      status: 400,
+      message: `Order id does not match route id. Order: ${id}, Route: ${orderId}`,
+    });
+  }
+
+  if (
+    !status ||
+    !["pending", "preparing", "out-for-delivery", "delivered"].includes(status)
+  ) {
+    return next({
+      status: 400,
+      message: `Order must have a status of pending, preparing, out-for-delivery, delivered`,
+    });
+  }
+  next();
+};
+
+const validateStatus = (req, res, next) => {
+  if (res.locals.order.status !== "pending") {
+    return next({
+      status: 400,
+      message: "An order cannot be deleted unless it is pending",
+    });
+  }
+  next();
+};
+
+const list = (req, res) => {
+  res.json({ data: orders });
+};
+
+const read = (req, res) => {
+  res.json({ data: res.locals.order });
+};
+
+const update = (req, res) => {
+  if (res.locals.data.hasOwnProperty("id")) {
+    delete res.locals.data.id;
+  }
+  const response = Object.assign(res.locals.order, res.locals.data);
+  res.json({ data: response });
+};
+
+const create = (req, res) => {
+  const { data: { deliverTo, mobileNumber, dishes } = {} } = req.body;
+  const newOrder = {
+    deliverTo,
+    mobileNumber,
+    dishes,
+    id: nextId(),
+  };
+  orders.push(newOrder);
+  res.status(201).json({ data: newOrder });
+};
+
+const deleter = (req, res) => {
+  orders.splice(orders.indexOf(res.locals.order), 1);
+  res.sendStatus(204);
+};
+
 // TODO: Implement the /orders handlers needed to make the tests pass
+module.exports = {
+  list,
+  create: [requiredFieldsCheck, create],
+  read: [orderExists, read],
+  update: [orderExists, requiredFieldsCheck, idValidator, update],
+  delete: [orderExists, validateStatus, deleter],
+};
